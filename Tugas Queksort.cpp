154016e370161a0156db78f14b8624d4819bda18
I Wayan Rio Darmawan <164158145+Riodarmawan1068@users.noreply.github.com>
2024-05-02 20:06:47 +0800

    Add files via upload

---
+++ Tugas Queksort.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+
+int partition(std::vector<int>& arr, int low, int high) {
+    int pivot = arr[high]; 
+    int i = low - 1; 
+
+    for (int j = low; j <= high - 1; j++) {
+        
+        if (arr[j] <= pivot) {
+            i++; 
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    std::swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+
+void quickSort(std::vector<int>& arr, int low, int high) {
+    if (low < high) {
+        
+        int pi = partition(arr, low, high);
+
+        
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main(int argc, char* argv[]) {
+        
+    std::vector<int> arr;
+    for (int i = 1; i < argc; i++) {
+        arr.push_back(std::atoi(argv[i]));
+    }
+
+    
+    quickSort(arr, 0, arr.size() - 1);
+
+    
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < arr.size(); i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
